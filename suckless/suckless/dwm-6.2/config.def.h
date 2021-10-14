a1780e0bde547674b1060af4f12041eeeb9dff77
astrolul <85197489+astrolul@users.noreply.github.com>
2021-10-14 21:11:20 +0100

    removed picom

--- suckless/suckless/dwm-6.2/config.def.h
+++ suckless/suckless/dwm-6.2/config.def.h
@@ -7,11 +7,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack Nerd Font:size=12" };
 static const char dmenufont[]       = "Hack Nerd Font:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#282a36";
+static const char col_gray2[]       = "#44475a";
+static const char col_gray3[]       = "#f8f8f2";
+static const char col_gray4[]       = "#282a36";
+static const char col_cyan[]        = "#bd93f9";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
