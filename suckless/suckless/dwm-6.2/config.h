6a8297ad31dee1ffb7ebde0f55a2837ed197871d
astrolul <85197489+astrolul@users.noreply.github.com>
2021-10-16 13:19:35 +0100

    added swallow patch

--- suckless/suckless/dwm-6.2/config.h
+++ suckless/suckless/dwm-6.2/config.h
@@ -4,6 +4,7 @@
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack Nerd Font:size=15" };
@@ -27,8 +28,8 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "st",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 };
 
 /* layout(s) */
