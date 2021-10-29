6fd8299608fff6cf334c53e2d96bd718733edddd
astrolul <85197489+astrolul@users.noreply.github.com>
2021-10-29 18:19:13 +0100

    changed colorscheme

--- suckless/suckless/st-0.8.4/config.h
+++ suckless/suckless/st-0.8.4/config.h
@@ -95,30 +95,29 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-
   /* 8 normal colors */
-  [0] = "#000000", /* black   */
-  [1] = "#ff5555", /* red     */
-  [2] = "#50fa7b", /* green   */
-  [3] = "#f1fa8c", /* yellow  */
-  [4] = "#bd93f9", /* blue    */
-  [5] = "#ff79c6", /* magenta */
-  [6] = "#8be9fd", /* cyan    */
-  [7] = "#bbbbbb", /* white   */
+  [0] = "#1d1f21", /* black   */
+  [1] = "#e1373c", /* red     */
+  [2] = "#61bc44", /* green   */
+  [3] = "#fdb922", /* yellow  */
+  [4] = "#009edd", /* blue    */
+  [5] = "#973b98", /* magenta */
+  [6] = "#009edd", /* cyan    */
+  [7] = "#707880", /* white   */
 
   /* 8 bright colors */
-  [8]  = "#44475a", /* black   */
-  [9]  = "#ff5555", /* red     */
-  [10] = "#50fa7b", /* green   */
-  [11] = "#f1fa8c", /* yellow  */
-  [12] = "#bd93f9", /* blue    */
-  [13] = "#ff79c6", /* magenta */
-  [14] = "#8be9fd", /* cyan    */
-  [15] = "#ffffff", /* white   */
+  [8]  = "#373b41", /* black   */
+  [9]  = "#e1373c", /* red     */
+  [10] = "#61bc44", /* green   */
+  [11] = "#fdb922", /* yellow  */
+  [12] = "#009edd", /* blue    */
+  [13] = "#973b98", /* magenta */
+  [14] = "#009edd", /* cyan    */
+  [15] = "#c5c8c6", /* white   */
 
   /* special colors */
-  [256] = "#282a36", /* background */
-  [257] = "#f8f8f2", /* foreground */
+  [256] = "#1d1f21", /* background */
+  [257] = "#c5c8c6", /* foreground */
 };
 
 /*
