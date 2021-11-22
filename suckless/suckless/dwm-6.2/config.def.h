07e62b7727feba50f00dba564cbbe27ff4f84332
astrolul <85197489+astrolul@users.noreply.github.com>
2021-11-22 19:15:00 +0000

    patched dwm so there is no transparent window borders

--- suckless/suckless/dwm-6.2/config.def.h
+++ suckless/suckless/dwm-6.2/config.def.h
@@ -7,8 +7,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack Nerd Font:size=15" };
-static const char dmenufont[]       = "Hack Nerd Font:size=15";
+static const char *fonts[]          = { "Hack Nerd Font:size=12" };
+static const char dmenufont[]       = "Hack Nerd Font:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#c8ccd4";
