5638407b36f4c8ce69ada4fd7dce568b96ca69ed
astrolul <85197489+astrolul@users.noreply.github.com>
2022-03-10 12:53:24 +0000

    added winicon patch

--- suckless/suckless/dwm-6.3/config.h
+++ suckless/suckless/dwm-6.3/config.h
@@ -6,6 +6,8 @@ static const unsigned int gappx     = 3;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+#define ICONSIZE 16   /* icon size */
+#define ICONSPACING 5 /* space between icon and title */
 static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "Terminus:size=16" };
 static const char dmenufont[]       = "Terminus:size=16";
