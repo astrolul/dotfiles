adbbd49d0696c85d83f32da1ea0ac17cad686306
astrolul <85197489+astrolul@users.noreply.github.com>
2022-03-11 12:48:18 +0000

    added hide_vacant_tags patch

--- suckless/suckless/dwm-6.2/config.h
+++ suckless/suckless/dwm-6.2/config.h
@@ -6,6 +6,8 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+#define ICONSIZE 26   /* icon size */
+#define ICONSPACING 8 /* space between icon and title */
 static const int user_bh            = 32;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "Terminus:style=bold:size=16" };
 static const char col_gray1[]       = "#222222";
