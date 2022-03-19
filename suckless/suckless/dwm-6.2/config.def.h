871a1f841a0e39f7e98b7f60ef0bc4d44d59b1ea
astrolul <85197489+astrolul@users.noreply.github.com>
2022-03-19 12:21:49 +0000

    update

--- suckless/suckless/dwm-6.2/config.def.h
+++ suckless/suckless/dwm-6.2/config.def.h
@@ -30,7 +30,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Gimp",     NULL,       NULL,       0,            0,           -1 }
 };
 
 /* layout(s) */
