37a5d7735475c09d850f6a9a646d104f60aebeca
astrolul <85197489+astrolul@users.noreply.github.com>
2025-09-26 00:24:35 +0100

    fixed dmenu

--- suckless/suckless/dmenu-5.4/config.h
+++ suckless/suckless/dmenu-5.4/config.h
@@ -3,10 +3,10 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const int user_bh = 12;               /* add an defined amount of pixels to the bar height */
+static const int user_bh = 8;               /* add an defined amount of pixels to the bar height */
 
 static const char *fonts[] = {
-	"TerminessNerdFont-Regular:size=21"
+	"TerminessNerdFont-Regular:size=16"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
