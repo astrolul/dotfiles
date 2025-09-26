bc3dfed44327d9ed252271b2943aac673e810896
astrolul <85197489+astrolul@users.noreply.github.com>
2025-09-26 01:57:49 +0100

    ok

--- suckless/suckless/st-0.9.3/config.h
+++ suckless/suckless/st-0.9.3/config.h
@@ -6,12 +6,6 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "TerminessNerdFont-Regular:pixelsize=30:antialias=true:autohint=true";
-/* Spare fonts */
-static char *font2[] = {
-	"PowerlineSymbols:style=Medium:pixelsize=30:antialias=true:autohint=true",
-/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
-};
-
 static int borderpx = 5;
 
 /*
