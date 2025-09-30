03186b2620260fe12e1a86b6ee014db35a15aab7
astrolul <85197489+astrolul@users.noreply.github.com>
2025-09-30 22:13:06 +0100

    added screenshot hotkey

--- suckless/suckless/dwm-6.6/config.def.h
+++ suckless/suckless/dwm-6.6/config.def.h
@@ -67,6 +67,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *roficmd[] = { "rofi", "-modi", "drun", "-show", "drun", "-theme", "/home/astrolul/.config/rofi/themes/merah.rasi", "-show-icons", "-steal-focus", NULL };
 static const char *rofiemojicmd[] = { "rofi", "-modi", "emoji", "-show", "emoji", "-theme", "/home/astrolul/.config/rofi/themes/merah.rasi", "-steal-focus", NULL };
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
+static const char *screenshotcmd[] = {"flameshot", "gui", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include "movestack.c"
@@ -74,6 +75,7 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ 0, XK_F9,               spawn,          {.v = brupcmd} },
     { 0, XK_F8,             spawn,          {.v = brdowncmd} },
+	{ 0, XK_F12,			spawn,			{.v = screenshotcmd} },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,						XK_space,  spawn,	   		{.v = roficmd } },
 	{ MODKEY,						XK_e,	   spawn,			{.v = rofiemojicmd } },
