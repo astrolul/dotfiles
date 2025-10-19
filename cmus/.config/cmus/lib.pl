a2e237bdcb60a44988298ce6bfc468725acd8f03
astrolul <85197489+astrolul@users.noreply.github.com>
2025-10-19 01:21:10 +0100

    added cmus

---
+++ cmus/.config/cmus/lib.pl
@@ -0,0 +1,65 @@
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 1/01. Method Man - Tical.flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 1/02. Method Man - Biscuits.flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 1/03. Method Man - Bring The Pain.flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 1/04. Method Man - All I Need (Original Album Version).flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 1/05. Method Man - What The Blood Clot.flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 1/06. Method Man - Meth Vs. Chef.flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 1/07. Method Man - Sub Crazy.flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 1/08. Method Man - Release Yo' Delf.flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 1/09. Method Man - P.L.O. Style.flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 1/10. Method Man - I Get My Thang In Action.flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 1/11. Method Man - Mr. Sandman.flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 1/12. Method Man - Stimulation.flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 1/13. Method Man - Method Man (Remix).flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 2/01. Method Man - Release Yo' Delf (Prodigy Mix).flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 2/02. Method Man - Release Yo' Delf (New Blood Mix).flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 2/03. Method Man - I'll Be There For You  You're All I Need To Get By (Razor Sharp Mix).flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 2/04. Method Man - I'll Be There For You You're All I Need To Get By (Razor Sharp Instrumental).flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 2/05. Method Man - I'll Be There For YouYou're All I Need To Get By (Puff Daddy Mix).flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 2/06. Method Man - I'll Be There For YouYou're All I Need To Get By (Puff Daddy Instrumental).flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 2/07. Method Man - I'll Be There For YouYou're All I Need To Get By (Soul Inside Edit).flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 2/08. Method Man - I'll Be There For YouYou're All I Need To Get By (Keep It Tight Mix).flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 2/09. Method Man - Bring The Pain (Chemical Brothers Remix).flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 2/10. Method Man - Bring The Pain (Remix).flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 2/11. Method Man - Bring The Pain (Instrumental).flac
+/home/astrolul/music/Method Man - Tical (1994) [FLAC] [16B-44.1kHz]/Disc 2/13. Capleton - Wings Of The Morning (Dynamik Duo Mix).flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 1/01. The Pharcyde - Bullshit.flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 1/02. The Pharcyde - Pharcyde.flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 1/03. The Pharcyde - Groupie Therapy.flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 1/04. The Pharcyde - Runnin'.flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 1/05. The Pharcyde - She Said.flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 1/06. The Pharcyde - Splattitorium.flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 1/07. The Pharcyde - Somethin' That Means Somethin'.flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 1/08. The Pharcyde - All Live.flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 1/09. The Pharcyde - Drop.flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 1/10. The Pharcyde - Hey You.flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 1/11. The Pharcyde - Y.flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 1/12. The Pharcyde - It's All Good!.flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 1/13. The Pharcyde - Moment In Time.flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 1/14. The Pharcyde - The Hustle.flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 1/15. The Pharcyde - Little D.flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 1/16. The Pharcyde - Devil Music.flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 1/17. The Pharcyde - The E.N.D.flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 2/01. The Pharcyde - Bullshit (Instrumental).flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 2/02. The Pharcyde - Pharcyde (Instrumental).flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 2/03. The Pharcyde - Groupie Therapy (Instrumental).flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 2/04. The Pharcyde - Runnin' (Instrumental).flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 2/05. The Pharcyde - Somethin' That Means Somethin' (Instrumental).flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 2/06. The Pharcyde - Drop (Instrumental).flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 2/07. The Pharcyde - Devil Music (Instrumental).flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 2/08. The Pharcyde - The E.N.D. (Instrumental).flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 2/09. The Pharcyde - Y (Remix Instrumental).flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 2/10. The Pharcyde - Drop (The Beatminerz Remix  Instrumental).flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 2/11. The Pharcyde - She Said (Remix Instrumental).flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 2/12. The Pharcyde - Runnin' (Philippians Remix Instrumental).flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 3/01. The Pharcyde - Just Don't Matter.flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 3/02. The Pharcyde - She Said (Mike Caren Remix).flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 3/03. The Pharcyde - Drop (The Beatminerz Remix).flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 3/04. The Pharcyde - Pandemonium.flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 3/05. The Pharcyde - Y (Be Like That) (Jay Dee Remix).flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 3/06. The Pharcyde - She Said (Jay Dee Remix).flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 3/07. The Pharcyde - Emerald Butterfly.flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 3/08. The Pharcyde - Runnin' (Rae & Christian Remix).flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 3/09. The Pharcyde - She Said (Fuzz Face & tWANK boy Remix).flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 3/10. The Pharcyde - Runnin' (Philippians RMX).flac
+/home/astrolul/music/The Pharcyde - Labcabincalifornia (1995) [FLAC] [16B-44.1kHz]/Disc 3/11. The Pharcyde - My Soul.flac
