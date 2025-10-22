2527a0fac0a3fe0eee813cc5f12e4521b55af788
astrolul <85197489+astrolul@users.noreply.github.com>
2025-10-22 03:10:48 +0100

    ok

---
+++ cmus/.config/cmus/lib.pl
@@ -0,0 +1,15 @@
+/home/astrolul/music/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition]/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition] - 01-01 Bring Da Ruckus.flac
+/home/astrolul/music/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition]/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition] - 01-02 Shame On a Nigga.flac
+/home/astrolul/music/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition]/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition] - 01-03 Clan In Da Front.flac
+/home/astrolul/music/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition]/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition] - 01-04 Wu-Tang_ 7th Chamber.flac
+/home/astrolul/music/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition]/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition] - 01-05 Can It Be All So Simple _ Intermission.flac
+/home/astrolul/music/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition]/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition] - 01-06 Da Mystery of Chessboxin'.flac
+/home/astrolul/music/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition]/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition] - 01-07 Wu-Tang Clan Ain't Nuthing ta F' Wit.flac
+/home/astrolul/music/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition]/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition] - 01-08 C.R.E.A.M. (Cash Rules Everything Around Me).flac
+/home/astrolul/music/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition]/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition] - 01-09 Method Man.flac
+/home/astrolul/music/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition]/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition] - 01-10 Protect Ya Neck.flac
+/home/astrolul/music/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition]/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition] - 01-11 Tearz.flac
+/home/astrolul/music/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition]/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition] - 01-12 Wu-Tang_ 7th Chamber - Part II.flac
+/home/astrolul/music/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition]/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition] - 01-13 Protect Ya Neck.flac
+/home/astrolul/music/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition]/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition] - 01-14 Method Man.flac
+/home/astrolul/music/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition]/Wu-Tang Clan - Enter The Wu-Tang (36 Chambers) [Expanded Edition] - 01-15 C.R.E.A.M. (Cash Rules Everything Around Me).flac
